0b3fc988708218c9e76583a08297ed011b36009a
岡田清志郎 <>
2020-11-10 21:07:33 +0900

    引数で出力ファイル指定

--- jikko.sh
+++ jikko.sh
@@ -1,1 +1,1 @@
-./a.out ../input/initial_pos_lipid.cdv ../input/initial_vel_lipid.cdv ../input/bond_info.cdv ../input/angle_info.cdv 
+./a.out ../input/initial_pos_lipid.cdv ../input/initial_vel_lipid.cdv ../input/bond_info.cdv ../input/angle_info.cdv          ../output/initial_pos_lipid.cdv ../output/initial_vel_lipid.cdv ../output/bond_info.cdv ../output/angle_info.cdv         ../output/detail.txt
